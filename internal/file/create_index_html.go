4a033b1072339f530262d9889d30ef94ccccf12b
dudung <dudung@gmail.com>
2022-10-01 20:23:28 +0700

    fin

--- internal/file/create_index_html.go
+++ internal/file/create_index_html.go
@@ -14,7 +14,7 @@ import (
     "os"
 )
 
-func main() {
+func write_index_html() {
   fname := "index.html"
   dest, err := os.Create(fname)
   if err != nil {
@@ -27,9 +27,30 @@ func main() {
   fmt.Fprintf(dest, "<html lang='en'>\n")
   fmt.Fprintf(dest, "  <head>\n")
   fmt.Fprintf(dest, "    <title>Hello Wordl</title>\n")
+  fmt.Fprintf(dest, "    <link rel='stylesheet' href='style.css' />\n")
   fmt.Fprintf(dest, "  </head>\n")
   fmt.Fprintf(dest, "  <body>\n")
   fmt.Fprintf(dest, "    <h1>Hello, Wordl!</h1>\n")
+  fmt.Fprintf(dest, "    <p>Sat, 1 Oct 2022</p>\n")
   fmt.Fprintf(dest, "  </body>\n")
   fmt.Fprintf(dest, "</html>\n")
+}
+
+func write_style_css() {
+  fname := "style.css"
+  dest, err := os.Create(fname)
+  if err != nil {
+    fmt.Println("os.Create:", err)
+    return
+  }
+  defer dest.Close()
+  
+  fmt.Fprintf(dest, "h1 {\n")
+  fmt.Fprintf(dest, "  color: blue;\n")
+  fmt.Fprintf(dest, "}\n")
+}
+
+func main() {
+  write_index_html()
+  write_style_css()
 }
